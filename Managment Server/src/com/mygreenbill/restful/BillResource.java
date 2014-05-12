4f76c39a503a70d039292ce7ed77d8cd7fcc894a
Jacob85 <yaki.ams@gmail.com>
2014-05-12 22:20:34 +0300

    implemented Restrul web services added another json

--- Managment Server/src/com/mygreenbill/restful/BillResource.java
+++ Managment Server/src/com/mygreenbill/restful/BillResource.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -25,6 +26,7 @@ public class BillResource
 {
     private final static Logger LOGGER = Logger.getLogger(BillResource.class);
     private final String getAllUserBills = "call mygreenbilldb.getalluserincomingmassages(?);";
+    private final String getAllUserBillsFromCompany = "call mygreenbilldb.getalluserincomingmassagesfromcompany(?, ?);";
 
     @GET @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
@@ -64,8 +66,56 @@ public class BillResource
      return null;
     }
 
+    @GET @Path("/fromCompany")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllUserBillsFromCompany(@Context HttpServletRequest request, @QueryParam("companyId") int companyId)
+    {
+
+        HttpSession session = request.getSession();
+        GreenBillUser greenBillUser = (GreenBillUser) session.getAttribute("user");
+        if (greenBillUser == null)
+        {
+            LOGGER.info("Cannot get user bills, user need to login again");
+            return errorJson("Please login again");
+        }
+        if (companyId == 0)
+        {
+            // this query param was not arrived
+            LOGGER.info("Cannot get company id ");
+            return errorJson("Please company id");
+        }
+        //user loged in, get all the user bills
+        String queryString = getAllUserBillsFromCompany.replaceFirst("\\?", greenBillUser.getUserId());
+        queryString = queryString.replace("?", String.valueOf(companyId));
+        DatabaseHandler databaseHandler = DatabaseHandler.getInstance();
+
+        try
+        {
+            List bills = databaseHandler.runGetQuery(queryString);
+            if (bills != null && bills.size() > 0)
+            {
+                JSONArray jsonBills = new JSONArray();
+                for (int i=0 ; i < bills.size(); i++)
+                {
+                    jsonBills.put(new JSONObject((Map)bills.get(i)));
+                }
+                return jsonBills.toString();
+            }
+            else
+            {
+                return errorJson("user has no messages");
+            }
+        } catch (DatabaseException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     private String errorJson(String message)
     {
-        return "{error: " + message + "}";
+        return "{error: " + message + "," +
+                "redirect: http://www.mygreenbill.com}";
     }
 }
