f2ace18b4cbfa09255f979438b70c73c91e365f7
Jacob85 <yaki.ams@gmail.com>
2014-05-24 10:38:07 +0300

    Added 2 more bills parsers to Cellcom and Yad2, and created  unittesting to test the parsers

---
+++ Copy To DB/src/com/mygreenbill/statistics/Yad2BillParser.java
@@ -0,0 +1,51 @@
+package com.mygreenbill.statistics;
+
+import org.apache.log4j.Logger;
+import org.apache.lucene.document.Document;
+import org.apache.pdfbox.lucene.LucenePDFDocument;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Jacob on 5/24/14.
+ */
+public class Yad2BillParser implements BillParser
+{
+    private static Logger LOGGER = Logger.getLogger(Yad2BillParser.class);
+    @Override
+    public double parseTotalAmountToPayFromPdf(String pathToFile)
+    {
+        Document luceneDocument = null;
+        try
+        {
+            luceneDocument = LucenePDFDocument.getDocument(new File(pathToFile));
+            LOGGER.info("File" + pathToFile + " as a String \r\n " + luceneDocument.toString());
+            String summery = luceneDocument.get("summary");
+            LOGGER.info("Summery:" + summery);
+            String[] splited = summery.split("\r\n");
+
+            System.out.println("looping...........");
+            for(String str : splited)
+            {
+                if (str.contains("לתשלום"))
+                {
+                    System.out.println(str);
+                    str = str.replaceAll("[^\\d.]", "");
+                    System.out.println(str);
+                    str = new StringBuilder(str).reverse().toString();
+                    String[] split = str.split("\\.");
+
+                    double num = Double.parseDouble(new StringBuilder(split[0]).toString());
+                    LOGGER.info("The Value parsed is: " + num);
+                    return num;
+                }
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+}
---
+++ Copy To DB/test/com/mygreenbill/billparser/BillParserTest.java
@@ -0,0 +1,31 @@
+package com.mygreenbill.billparser;
+
+import com.mygreenbill.statistics.BillParser;
+import com.mygreenbill.statistics.CellcomBillParser;
+import com.mygreenbill.statistics.OrangeBillParser;
+import com.mygreenbill.statistics.Yad2BillParser;
+import junit.framework.TestCase;
+
+import java.io.File;
+
+/**
+ * Created by Jacob on 5/24/14.
+ */
+public class BillParserTest extends TestCase
+{
+    public void testParseTotalAmountToPayFromPdf() throws Exception
+    {
+        System.out.println(new File(".").getAbsolutePath());
+        BillParser billParser = new Yad2BillParser();
+        double amount = billParser.parseTotalAmountToPayFromPdf("./Copy To DB/conf/bills/yad2.pdf");
+        assertEquals(amount, 12.0);
+
+        billParser = new CellcomBillParser();
+        amount = billParser.parseTotalAmountToPayFromPdf("./Copy To DB/conf/bills/cellcom.pdf");
+        assertEquals(amount, 112.0);
+
+        billParser = new OrangeBillParser();
+        amount = billParser.parseTotalAmountToPayFromPdf("./Copy To DB/conf/bills/orange.pdf");
+        assertEquals(amount, 173.0);
+    }
+}
